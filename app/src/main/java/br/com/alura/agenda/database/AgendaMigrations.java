9bc2da82babb506044ad0378fae4c90e97584576
Alex Felipe <alexfelipevieira@gmail.com>
2019-02-12 11:04:02 -0200

    2.3

--- app/src/main/java/br/com/alura/agenda/database/AgendaMigrations.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaMigrations.java
@@ -3,6 +3,9 @@ package br.com.alura.agenda.database;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.room.migration.Migration;
 import androidx.annotation.NonNull;
+import br.com.alura.agenda.model.TipoTelefone;
+
+import static br.com.alura.agenda.model.TipoTelefone.FIXO;
 
 class AgendaMigrations {
 
@@ -58,6 +61,35 @@ class AgendaMigrations {
             database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
         }
     };
-    static final Migration[] TODAS_MIGRATIONS = {MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5};
+    private static final Migration MIGRATION_5_6 = new Migration(5, 6) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo` (" +
+                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`nome` TEXT, " +
+                    "`email` TEXT, " +
+                    "`momentoDeCadastro` INTEGER)");
+
+            database.execSQL("INSERT INTO Aluno_novo (id, nome, email, momentoDeCadastro) " +
+                    "SELECT id, nome, email, momentoDeCadastro FROM Aluno");
+
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Telefone` (" +
+                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`numero` TEXT, " +
+                    "`tipo` TEXT, " +
+                    "`alunoId` INTEGER NOT NULL)");
+
+            database.execSQL("INSERT INTO Telefone (numero, alunoId) " +
+                    "SELECT telefoneFixo, id FROM Aluno");
+
+            database.execSQL("UPDATE Telefone SET tipo = ?", new TipoTelefone[] {FIXO});
+
+            database.execSQL("DROP TABLE Aluno");
+
+            database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
+        }
+    };
+    static final Migration[] TODAS_MIGRATIONS = {MIGRATION_1_2, MIGRATION_2_3,
+            MIGRATION_3_4, MIGRATION_4_5, MIGRATION_5_6};
 
 }
