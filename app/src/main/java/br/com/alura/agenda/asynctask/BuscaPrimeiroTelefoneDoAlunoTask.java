5f7a10c811fc4f276f167d216c67ca4b2ce6c305
Alex Felipe <alexfelipevieira@gmail.com>
2019-02-15 09:47:08 -0200

    5.1

---
+++ app/src/main/java/br/com/alura/agenda/asynctask/BuscaPrimeiroTelefoneDoAlunoTask.java
@@ -0,0 +1,37 @@
+package br.com.alura.agenda.asynctask;
+
+import android.os.AsyncTask;
+
+import br.com.alura.agenda.database.dao.TelefoneDAO;
+import br.com.alura.agenda.model.Telefone;
+
+public class BuscaPrimeiroTelefoneDoAlunoTask extends AsyncTask<Void, Void, Telefone> {
+
+    private final TelefoneDAO dao;
+    private final int alunoId;
+    private final PrimeiroTelefoneEncontradoListener listener;
+
+    public BuscaPrimeiroTelefoneDoAlunoTask(TelefoneDAO dao,
+                                            int alunoId,
+                                            PrimeiroTelefoneEncontradoListener listener) {
+        this.dao = dao;
+        this.alunoId = alunoId;
+        this.listener = listener;
+    }
+
+    @Override
+    protected Telefone doInBackground(Void... voids) {
+        return dao.buscaPrimeiroTelefoneDoAluno(alunoId);
+    }
+
+    @Override
+    protected void onPostExecute(Telefone primeiroTelefone) {
+        super.onPostExecute(primeiroTelefone);
+        listener.quandoEncontrado(primeiroTelefone);
+    }
+
+    public interface PrimeiroTelefoneEncontradoListener {
+        void quandoEncontrado(Telefone telefoneEncontrado);
+    }
+
+}
--- app/src/main/java/br/com/alura/agenda/ui/adapter/ListaAlunosAdapter.java
+++ app/src/main/java/br/com/alura/agenda/ui/adapter/ListaAlunosAdapter.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.alura.agenda.R;
-import br.com.alura.agenda.asynctask.BuscaPrimeiroTelefoneDoAluno;
+import br.com.alura.agenda.asynctask.BuscaPrimeiroTelefoneDoAlunoTask;
 import br.com.alura.agenda.database.AgendaDatabase;
 import br.com.alura.agenda.database.dao.TelefoneDAO;
 import br.com.alura.agenda.model.Aluno;
@@ -54,8 +54,9 @@ public class ListaAlunosAdapter extends BaseAdapter {
         TextView nome = view.findViewById(R.id.item_aluno_nome);
         nome.setText(aluno.getNome());
         TextView telefone = view.findViewById(R.id.item_aluno_telefone);
-        new BuscaPrimeiroTelefoneDoAluno(dao,
-                telefone, aluno.getId()).execute();
+        new BuscaPrimeiroTelefoneDoAlunoTask(dao, aluno.getId(),
+                telefoneEncontrado ->
+                        telefone.setText(telefoneEncontrado.getNumero())).execute();
     }
 
     private View criaView(ViewGroup viewGroup) {
@@ -64,7 +65,7 @@ public class ListaAlunosAdapter extends BaseAdapter {
                 .inflate(R.layout.item_aluno, viewGroup, false);
     }
 
-    public void atualiza(List<Aluno> alunos){
+    public void atualiza(List<Aluno> alunos) {
         this.alunos.clear();
         this.alunos.addAll(alunos);
         notifyDataSetChanged();
